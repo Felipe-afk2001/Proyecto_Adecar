34e20a0c351243397dc0baf82a1319b9b50a50e4
Sebastián Riveros <seba.arp@outlook.com>
2023-10-22 01:08:57 -0300

    Arregladas las rutas

--- Sistemas/Calculo_auto/views.py
+++ Sistemas/Calculo_auto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def home1 (request):
-    return render(request, 'Calculo_auto/main_menu.html')
+    return render(request, 'main_calculo_auto.html')
 # Create your views here.
--- Sistemas/Core/views.py
+++ Sistemas/Core/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
 
 #Definir las vistas de lo que queremos hacer y su lógica.
-
-def main_menu(request):
-    return render(request, 'main_menu.html', {})
