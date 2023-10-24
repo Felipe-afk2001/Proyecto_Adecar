9effa2016a8d08d5ecf19193e683ffc5a660120b
felipe-afk2001 <fe.medinaro@duocuc.cl>
2023-10-24 17:31:57 -0300

    registrar usuarios funcionando

--- Sistemas/Calculo_Cotizaciones/views.py
+++ Sistemas/Calculo_Cotizaciones/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from .forms import CustomAuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')  # Redirige a la vista de inicio después del inicio de sesión
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 def inicio (request):
     return render(request, 'inicio.html')
 
-class CustomLoginView(LoginView):
-    authentication_form = CustomAuthenticationForm
-    
+def registrar(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'La cuenta ha sido creada para {username}')
+            return redirect('login')  # Puedes cambiar 'login' por la URL de inicio de sesión de tu aplicación
+    else:
+        form = UserCreationForm()
+    return render(request, 'registrar.html', {'form': form})
+
 @login_required
 def home (request):
     return render(request, 'home.html')
 
+def registrar(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'La cuenta ha sido creada para {username}')
+            return redirect('login')  # Puedes cambiar 'login' por la URL de inicio de sesión de tu aplicación
+    else:
+        form = UserCreationForm()
+    return render(request, 'registrar.html', {'form': form})
 
