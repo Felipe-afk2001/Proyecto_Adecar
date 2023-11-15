36f82d424983a06e0d09168c84aa0220549f71e3
Sebastián Riveros <seba.arp@outlook.com>
2023-11-15 05:33:48 -0300

    Dashboard en camino

--- Sistemas/Calculo_Cotizaciones/urls.py
+++ Sistemas/Calculo_Cotizaciones/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import home, registrar, crear_plancha, login_view, eliminar_plancha, editar_plancha, lista_planchas, mantencion_planchas_form, crear_parametro, mantencion_parametros_form, cotizacion_manual, calculo_de_precio_2, procesar_datos, procesar_datos_2, calculo_de_precio, cotizacion_manual_2, editar_parametro, eliminar_parametro, lista_parametros
+from .views import home, registrar, dashboards, lista_historial, crear_plancha, login_view, eliminar_plancha, editar_plancha, lista_planchas, mantencion_planchas_form, crear_parametro, mantencion_parametros_form, cotizacion_manual, calculo_de_precio_2, procesar_datos, procesar_datos_2, calculo_de_precio, cotizacion_manual_2, editar_parametro, eliminar_parametro, lista_parametros
 from django.contrib.auth.views import LogoutView
+import Calculo_Cotizaciones.dash_apps
 
 
 urlpatterns = [
     path('',home, name='home'),
+    path('home/', home, name='home'),
     path('registrar/', registrar, name='registrar'),
     path('login/', login_view, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
@@ -24,6 +26,8 @@ urlpatterns = [
     path('eliminar_parametro/<int:id>/', eliminar_parametro, name='eliminar_parametro'),
     path('lista_parametros', lista_parametros, name='lista_parametros'),
     path('crear_parametro', crear_parametro, name='crear_parametro'),
-    # path('visualiza_pdf/', visualiza_pdf, name='visualiza_pdf'),
-    
+    path('lista_historial', lista_historial, name='lista_historial'),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('dashboards/', dashboards, name='dashboards'),
+    # path('visualiza_pdf/', visualiza_pdf, name='visualiza_pdf'),   
 ]
