2a5d28bd9fc7ef03725b2fb34280e33d05af07fe
Sebastián Riveros <seba.arp@outlook.com>
2023-11-16 17:54:58 -0300

    Agregar cliente semi listo

--- Sistemas/Calculo_Cotizaciones/urls.py
+++ Sistemas/Calculo_Cotizaciones/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import home, registrar, dashboards, lista_historial, crear_plancha, login_view, eliminar_plancha, editar_plancha, lista_planchas, mantencion_planchas_form, crear_parametro, mantencion_parametros_form, cotizacion_manual, calculo_de_precio_2, procesar_datos, procesar_datos_2, calculo_de_precio, cotizacion_manual_2, editar_parametro, eliminar_parametro, lista_parametros
+from .views import home, creacion_cliente, registrar, dashboards, lista_historial, crear_plancha, login_view, eliminar_plancha, editar_plancha, lista_planchas, mantencion_planchas_form, crear_parametro, mantencion_parametros_form, cotizacion_manual, procesar_datos, calculo_de_precio, editar_parametro, eliminar_parametro, lista_parametros
 from django.contrib.auth.views import LogoutView
 import Calculo_Cotizaciones.dash_apps
 
@@ -16,11 +16,8 @@ urlpatterns = [
     path('lista_planchas', lista_planchas, name='lista_planchas'),
     path('crear_plancha/', crear_plancha, name='crear_plancha'),
     path('cotizacion_manual/', cotizacion_manual, name='cotizacion_manual'),
-    path('cotizacion_manual_2/', cotizacion_manual_2, name='cotizacion_manual_2'),
     path('procesar_datos/', procesar_datos, name='procesar_datos'),
-    path('procesar_datos_2/', procesar_datos_2, name='procesar_datos_2'),
     path('calculo_de_precio/', calculo_de_precio, name='calculo_de_precio'),
-    path('calculo_de_precio_2/', calculo_de_precio_2, name='calculo_de_precio_2'),
     path('mantencion_parametros_form/', mantencion_parametros_form, name='mantencion_parametros_form'),
     path('editar_parametro/<int:id>/', editar_parametro, name='editar_parametro'),
     path('eliminar_parametro/<int:id>/', eliminar_parametro, name='eliminar_parametro'),
@@ -29,5 +26,6 @@ urlpatterns = [
     path('lista_historial', lista_historial, name='lista_historial'),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
     path('dashboards/', dashboards, name='dashboards'),
+    path('creacion_cliente/', creacion_cliente, name='creacion_cliente'),
     # path('visualiza_pdf/', visualiza_pdf, name='visualiza_pdf'),   
 ]
--- Sistemas/Calculo_Cotizaciones/views.py
+++ Sistemas/Calculo_Cotizaciones/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
 from Calculo_Cotizaciones.models import Parametro #Para hacer las validaciones de los parámetros
 from django.http import JsonResponse, HttpResponse
-from Calculo_Cotizaciones.models import Tipo_Plancha, Historial
+from Calculo_Cotizaciones.models import Tipo_Plancha, Historial, Cliente
 from django.db import connection
 from django.core.paginator import Paginator
 import Calculo_Cotizaciones.dash_apps
@@ -272,15 +272,41 @@ VISTAS DE COTIZACIÓN MANUAL
 def cotizacion_manual (request):
     return render(request, 'cotizacion_manual.html')
 
-def cotizacion_manual_2(request):
-    return render(request, 'cotizacion_manual_2.html')
-
 def calculo_de_precio(request):
     return render(request, 'calculo_de_precio.html')
 
-def calculo_de_precio_2(request):
-    return render(request, 'calculo_de_precio_2.html')
+def creacion_cliente(request):
+    return render(request, 'creacion_cliente.html')
 
+from django.shortcuts import render, redirect
+from .models import Cliente  # Importa el modelo Cliente
+from django.core.exceptions import ValidationError
+
+def agregar_cliente(request):
+    mensaje_error = None
+    if request.method == 'POST':
+        rut_cliente = request.POST.get('rut_cliente')
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        correo = request.POST.get('correo')
+
+        # Verifica si el RUT ya existe en la base de datos
+        if Cliente.objects.filter(rut_cliente=rut_cliente).exists():
+            mensaje_error = "El RUT ingresado ya existe en la base de datos."
+
+        if not mensaje_error:
+            try:
+                # Crear y guardar la nueva instancia de Cliente
+                nuevo_cliente = Cliente(rut_cliente=rut_cliente, nombre=nombre, apellido=apellido, correo=correo)
+                nuevo_cliente.save()
+
+                # Después de agregar el cliente, redirige a donde necesites
+                return redirect('calculo_de_precio')
+            except Exception as e:
+                mensaje_error = f"Error al agregar el cliente: {str(e)}"
+
+    # Si hay un mensaje de error o es una solicitud GET, muestra la plantilla con el mensaje
+    return render(request, 'agregar_cliente.html', {'mensaje_error': mensaje_error})
 
 
 """
@@ -291,6 +317,7 @@ PROCESAR DATOS FORMULARIO MANUAL
 def procesar_datos(request):
     mensaje_error = None #inicializador
     if request.method == 'POST':
+        rut_cliente = request.POST.get('rut_cliente')
         largo = float(request.POST.get('largo'))
         largostr = request.POST.get('largo')
         ancho = float(request.POST.get('ancho'))
@@ -299,7 +326,20 @@ def procesar_datos(request):
         altostr = request.POST.get('alto')
         cantidad = int(request.POST.get('cantidad_cajas'))
         tipo_carton = request.POST.get('tipo_carton').upper()
-        rut_cliente = request.POST.get('rut_cliente')
+
+         # Verificar si el cliente ya existe
+        try:
+            cliente = Cliente.objects.get(rut_cliente=rut_cliente)
+            # El cliente existe, logica continúa
+
+            #rut de arriba
+            nombre_cliente = cliente.nombre
+            apellido_cliente = cliente.apellido
+            correo_cliente = cliente.correo
+        except Cliente.DoesNotExist:
+            # El cliente no existe, redirige a una nueva plantilla para agregar al cliente
+            return redirect('creacion_cliente')
+
         
 
         """
@@ -520,176 +560,3 @@ def procesar_datos(request):
         #                                                   'costo_ex_vertical':round(costo_ex_vertical),
         #                                                   'costo_ex_horizontal':round(costo_ex_horizontal),
         #                                                   'costo_por_unidad':round(costo_por_unidad),})
-
-def procesar_datos_2(request):
-
-
-    mensaje_error = None #inicializador
-    if request.method == 'POST':
-        largo = float(request.POST.get('largo'))
-        largostr = request.POST.get('largo')
-        ancho = float(request.POST.get('ancho'))
-        anchostr = request.POST.get('ancho')
-        alto = float(request.POST.get('alto'))
-        altostr = request.POST.get('alto')
-        cantidad = int(request.POST.get('cantidad_cajas'))
-        tipo_carton = request.POST.get('tipo_carton').upper()
-        porcentajes = request.POST.get('porcentaje')
-
-        """
-        Calcula largo de hoja
-        """
-        la1=largo+3
-        la2=ancho+5
-        la3=largo+5
-        la4=ancho+3
-        la5=40 #Aleta
-        largo1=la1+la2+la3+la4+la5
-        largo2=la1+la2+la5 #media
-        """
-        Calcula ancho de hoja
-        """
-        an1=(ancho/2)+3
-        an2=alto+5
-        an3=(ancho/2)+3
-        ancho1=an1+an2+an3
-
-        """
-        Calcula hoja madre
-        """
-        largo_hm = largo1
-        ancho_hm = ancho1
-        media_hm = largo2
-
-        # Verifica los tipos de datos
-        print("Tipo de largo_hm:", type(largo_hm))
-        print("Tipo de ancho_hm:", type(ancho_hm))
-        print("Tipo de tipo_carton:", type(tipo_carton))
-
-        largo_hm_str = str(math.trunc(largo_hm))
-        ancho_hm_str = str(math.trunc(ancho_hm))
-        media_hm_str = str(math.trunc(media_hm))
-
-        """
-        Seleccionar plancha
-        """
-        # plancha_necesaria = seleccionar_plancha(largo_hm, media_hm, ancho_hm, tipo_carton)
-
-        plancha_seleccionada, total_cajas_por_plancha = calcular_cajas_por_plancha(largo_hm, media_hm, ancho_hm, tipo_carton)
-        print(total_cajas_por_plancha)
-
-        
-
-        """
-        Excedentes de plancha
-        """
-        largo_plancha = plancha_seleccionada.get('largo')
-        ancho_plancha = plancha_seleccionada.get('ancho')
-
-        # Calcula cuántas cajas caben en el ancho de la plancha (solo enteras)
-        cajas_en_ancho = ancho_plancha // ancho_hm
-
-        # Calcula cuántas cajas caben en el largo de la plancha (enteras y mitades)
-        cajas_enteras_en_largo = largo_plancha // largo_hm
-        espacio_restante = largo_plancha % largo_hm
-        cajas_en_largo = cajas_enteras_en_largo
-        if espacio_restante >= media_hm:
-            cajas_en_largo += 0.5  # Añade media caja si hay espacio
-
-        # Calcula el espacio total ocupado por las cajas en el largo y ancho
-        espacio_ocupado_en_ancho = cajas_en_ancho * ancho_hm
-        espacio_ocupado_en_largo = (cajas_enteras_en_largo * largo_hm) + (media_hm if espacio_restante >= media_hm else 0)
-        excedente_horizontal = ancho_plancha - espacio_ocupado_en_ancho
-        excedente_vertical = largo_plancha - espacio_ocupado_en_largo
-
-        """
-        Porcentajes de venta a cargar en próxima plantilla
-        """
-        porcentajes = list(range(5, 105, 5))
-
-        """
-        Validación de medida contra tabla de parámetros
-        """
-        parametros = Parametro.objects.first()
-        if largo1 > parametros.largo_maximo or ancho1 > parametros.ancho_maximo:
-            mensaje_error = "Medidas inválidas. Por favor, vuelva e ingrese otro valor."
-
-        """
-        Costo de excedentes
-        """
-        def calcular_costo_excedentes(excedente_vert, excedente_horiz, costo_plancha, area_plancha, umbral_vertical, umbral_horizontal):
-            costo_excedente_vertical = 0
-            costo_excedente_horizontal = 0
-
-            # Costo del excedente horizontal
-            if excedente_horiz > umbral_horizontal:
-                area_excedente_horizontal = excedente_horiz * ancho_plancha
-                proporcion_horizontal = area_excedente_horizontal / area_plancha
-                costo_excedente_horizontal = costo_plancha * proporcion_horizontal
-
-            # Costo del excedente vertical
-            if excedente_vert > umbral_vertical:
-                area_excedente_vertical = excedente_vert * ancho_plancha
-                proporcion_vertical = area_excedente_vertical / area_plancha
-                costo_excedente_vertical = costo_plancha * proporcion_vertical
-
-            return costo_excedente_vertical, costo_excedente_horizontal
-
-        
-
-
-        """
-        Llamada a la API
-        """
-        # Ejemplo de cómo llamar a la función
-        enviar_solicitud_api(largo, ancho, alto, plancha_seleccionada.get('largo'),plancha_seleccionada.get('ancho'),plancha_seleccionada.get('precio_proveedor'), cantidad)
-        precio_unidad = 100
-
-        """
-        Llamada costo excedentes
-        """
-        coste_materia = plancha_seleccionada.get('precio_proveedor')
-        excedente_horizontal_param, excedente_vertical_param = parametros_excedentes()
-        area_plancha_bd = plancha_seleccionada.get('area')
-        costo_ex_vertical, costo_ex_horizontal = calcular_costo_excedentes(
-            excedente_vert=excedente_vertical, 
-            excedente_horiz=excedente_horizontal,
-            costo_plancha=coste_materia, 
-            area_plancha=area_plancha_bd,
-            umbral_vertical=excedente_vertical_param,
-            umbral_horizontal=excedente_horizontal_param
-        )
-
-        """
-        Costo por unidad
-        """
-        # Asumiendo que total_cajas_por_plancha es el número total de cajas por plancha
-        coste_total_plancha = coste_materia - (costo_ex_vertical + costo_ex_horizontal)
-        costo_por_unidad = coste_total_plancha / total_cajas_por_plancha
-
-    if mensaje_error:
-        return render(request, 'cotizacion_manual_2.html', {'mensaje_error': mensaje_error})
-    else:
-        return render(request, 'calculo_de_precio_2.html',{'largo':largo,
-                                                          'ancho':ancho,
-                                                          'alto':alto,
-                                                          'cantidad':cantidad,
-                                                          'tipo_carton':tipo_carton,
-                                                          'porcentajes':porcentajes,
-                                                          'largostr':largostr,
-                                                          'anchostr':anchostr,
-                                                          'altostr':altostr,
-                                                          'largo_hm':largo_hm,
-                                                          'ancho_hm':ancho_hm,
-                                                          'media_hm':media_hm,
-                                                          'largo_hm_str':largo_hm_str,
-                                                          'ancho_hm_str':ancho_hm_str,
-                                                          'media_hm_str':media_hm_str,
-                                                          'plancha_necesaria':plancha_seleccionada,
-                                                          'excedente_vertical':excedente_vertical,
-                                                          'excedente_horizontal':excedente_horizontal,
-                                                          'total_cajas_por_plancha': total_cajas_por_plancha,
-                                                          'precio_base':precio_unidad,
-                                                          'costo_ex_vertical':round(costo_ex_vertical),
-                                                          'costo_ex_horizontal':round(costo_ex_horizontal),
-                                                          'costo_por_unidad':round(costo_por_unidad),})
