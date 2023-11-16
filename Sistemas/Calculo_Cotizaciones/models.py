23ca47d066baaaecedb0102c090c05c12f9d5420
Sebastián Riveros <seba.arp@outlook.com>
2023-11-16 16:41:55 -0300

    monto_total en solicitud de cotización agregado, esta tabla se usará para el historial y el dash, no borrar historial aún hasta redirigir las consultas de historial a solicitud

--- Sistemas/Calculo_Cotizaciones/models.py
+++ Sistemas/Calculo_Cotizaciones/models.py
@@ -50,6 +50,7 @@ class Solicitud_Cotizacion(models.Model):
     comentario = models.CharField(max_length=400)
     estado = models.CharField(max_length=50, default='Pendiente')
     fecha_cotizacion = models.DateTimeField(auto_now_add=True)
+    monto_total = models.DecimalField(max_digits=10, decimal_places=2)
 
     @property
     def nombre_cliente(self):
